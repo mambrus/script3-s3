a9d6b258d71bde45e8fa32eb5cd18eb96166a8fc
Michael Ambrus <mambrus@mambrus-laptop.(none)>
2011-02-19 03:31:14 +0100

    Made openoecd/install_and_setup.sh work

---
+++ test_install_bin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2011-01-22
+
+TEST_INSTALL_BIN_SH="test_install_bin.sh"
+
+# Tests for an executable, and if not found in path then install it
+function test_install_bin() {
+  if [ -z $(which $1) ]; then
+    set +e
+    ask_user_continue \
+	  "No [$1] found, install & continue? (Y/n)"\
+	  "Installing [$1]..."\
+	  "Can't continue without [$1]. Exiting..."
+    RC=$?
+    set -e
+    if [ $RC -eq 0 ]; then
+      sudo apt-get install $1
+      RC=$?
+    else
+      return 1
+    fi
+  fi
+  return $RC
+}
+
+if [ -z $EBASENAME_SH ]; then
+	source s3.ebasename.sh
+fi
+
+if [ "$TEST_INSTALL_BIN_SH" == $( ebasename $0 ) ]; then
+  #Not sourced, do something with this.
+  source user_response.sh
+
+  test_install_pkg "$@"
+  exit $?
+fi
---
+++ test_install_pkg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2011-01-22
+
+TEST_INSTALL_PKG_SH="test_install_pkg.sh"
+
+# Tests for a package, and if not installed then install it
+function test_install_pkg() {
+  local PACKV=$(\
+    dpkg-query -l "${1}" | \
+	grep "ii" | \
+	sed -e 's/^[[:graph:]]\+[[:space:]]\+[[:graph:]]\+[[:space:]]\+//' | \
+	cut -f1 -d" "\
+  );
+  #echo $PACKV
+
+  if [ -z $PACKV ]; then
+    set +e
+    ask_user_continue \
+	  "Package [$1] not installed, install & continue? (Y/n)"\
+	  "Installing [$1]..."\
+	  "Can't continue without [$1]. Exiting..."
+    RC=$?
+    set -e
+    if [ $RC -eq 0 ]; then
+      sudo apt-get install $1
+      RC=$?
+    else
+      return 1
+    fi
+  fi
+  return $RC
+}
+
+if [ -z $EBASENAME_SH ]; then
+	source s3.ebasename.sh
+fi
+
+if [ "$TEST_INSTALL_PKG_SH" == $( ebasename $0 ) ]; then
+  #Not sourced, do something with this.
+  source user_response.sh
+
+  test_install_pkg $@
+  exit $?
+fi
