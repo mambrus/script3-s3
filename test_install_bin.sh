98b6b41972055c51aa56ad5711dccf797943ad36
Michael Ambrus <michael.ambrus@gmail.com>
2011-02-19 04:03:14 +0100

    Costemtic change

--- test_install_bin.sh
+++ test_install_bin.sh
@@ -6,22 +6,22 @@ TEST_INSTALL_BIN_SH="test_install_bin.sh"
 
 # Tests for an executable, and if not found in path then install it
 function test_install_bin() {
-  if [ -z $(which $1) ]; then
-    set +e
-    ask_user_continue \
-	  "No [$1] found, install & continue? (Y/n)"\
-	  "Installing [$1]..."\
-	  "Can't continue without [$1]. Exiting..."
-    RC=$?
-    set -e
-    if [ $RC -eq 0 ]; then
-      sudo apt-get install $1
-      RC=$?
-    else
-      return 1
-    fi
-  fi
-  return $RC
+	if [ -z $(which $1) ]; then
+	set +e
+	ask_user_continue \
+		"No [$1] found, install & continue? (Y/n)"\
+		"Installing [$1]..."\
+		"Can't continue without [$1]. Exiting..."
+	RC=$?
+	set -e
+	if [ $RC -eq 0 ]; then
+		sudo apt-get install $1
+		RC=$?
+	else
+		return 1
+	fi
+	fi
+	return $RC
 }
 
 if [ -z $EBASENAME_SH ]; then
@@ -29,9 +29,9 @@ if [ -z $EBASENAME_SH ]; then
 fi
 
 if [ "$TEST_INSTALL_BIN_SH" == $( ebasename $0 ) ]; then
-  #Not sourced, do something with this.
-  source user_response.sh
+	#Not sourced, do something with this.
+	source user_response.sh
 
-  test_install_pkg "$@"
-  exit $?
+	test_install_pkg "$@"
+	exit $?
 fi
--- test_install_pkg.sh
+++ test_install_pkg.sh
@@ -6,30 +6,30 @@ TEST_INSTALL_PKG_SH="test_install_pkg.sh"
 
 # Tests for a package, and if not installed then install it
 function test_install_pkg() {
-  local PACKV=$(\
-    dpkg-query -l "${1}" | \
+	local PACKV=$(\
+	dpkg-query -l "${1}" | \
 	grep "ii" | \
 	sed -e 's/^[[:graph:]]\+[[:space:]]\+[[:graph:]]\+[[:space:]]\+//' | \
 	cut -f1 -d" "\
-  );
-  #echo $PACKV
+	);
+	#echo $PACKV
 
-  if [ -z $PACKV ]; then
-    set +e
-    ask_user_continue \
-	  "Package [$1] not installed, install & continue? (Y/n)"\
-	  "Installing [$1]..."\
-	  "Can't continue without [$1]. Exiting..."
-    RC=$?
-    set -e
-    if [ $RC -eq 0 ]; then
-      sudo apt-get install $1
-      RC=$?
-    else
-      return 1
-    fi
-  fi
-  return $RC
+	if [ -z $PACKV ]; then
+	set +e
+	ask_user_continue \
+		"Package [$1] not installed, install & continue? (Y/n)"\
+		"Installing [$1]..."\
+		"Can't continue without [$1]. Exiting..."
+	RC=$?
+	set -e
+	if [ $RC -eq 0 ]; then
+		sudo apt-get install $1
+		RC=$?
+	else
+		return 1
+	fi
+	fi
+	return $RC
 }
 
 if [ -z $EBASENAME_SH ]; then
@@ -37,9 +37,9 @@ if [ -z $EBASENAME_SH ]; then
 fi
 
 if [ "$TEST_INSTALL_PKG_SH" == $( ebasename $0 ) ]; then
-  #Not sourced, do something with this.
-  source user_response.sh
+	#Not sourced, do something with this.
+	source user_response.sh
 
-  test_install_pkg $@
-  exit $?
+	test_install_pkg $@
+	exit $?
 fi
