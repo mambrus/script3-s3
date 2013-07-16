38aa5967db1dd150ec311858521902f9118cf128
Michael Ambrus <michael.ambrus@gmail.com>
2013-07-16 16:27:18 +0200

    Colors and fonts
    Should be usable and importable into ./ui/*.sh now
    
    Change-Id: I95801ddb74440b0d814246321d6378e6865c0235

--- fonts.sh
+++ fonts.sh
@@ -6,34 +6,67 @@
 
 # tput sgr0
 
-# Note that escape can be written in different ways:
-# \033[
+# Note that escape sequence can be written in several different (valid) 
+# ways:
 # \e[
+# \033[
 # \x1b[
 
-# In some examples "01;" is inserted between the escpae and the code. I don't
-# know why and it doesn't seem to matter so it is skipped here (KISS).
 
-# Colors:
-FONT_RED='\033[01;31m'
-FONT_GREEN='\033[01;32m'
-FONT_YELLOW='\033[01;33m'
-FONT_PURPLE='\033[01;35m'
-FONT_CYAN='\033[01;36m'
-FONT_WHITE='\033[01;37m'
+# If pipe is open and is stdout (=1), only then
+if [ -t 1 ] ; then
+# Color (foreground):
+	BLACK='\e[30m'
+	BLUE='\e[34m'
+	GREEN='\e[32m'
+	CYAN='\e[36m'
+	RED='\e[31m'
+	PURPLE='\e[35m'
+	BROWN='\e[33m'
+	LIGHT_GRAY='\e[37m'
+	DARK_GRAY='\e[1;30m'
+	LIGHT_BLUE='\e[1;34m'
+	LIGHT_GREEN='\e[1;32m'
+	LIGHT_CYAN='\e[1;36m'
+	LIGHT_RED='\e[1;31m'
+	LIGHT_PURPLE='\e[1;35m'
+	YELLOW='\e[1;33m'
+	WHITE='\e[1;37m'
 
-     echo -e "\x1b[30m black"
-     echo -e "\x1b[31m red"
-     echo -e "\x1b[32m green"
-     echo -e "\x1b[33m yellow"
-     echo -e "\x1b[34m blue"
-     echo -e "\x1b[35m mag"
-     echo -e "\x1b[36m cyan"
-     echo -e "\x1b[37m white"
+# Color (explicit foreground):
+	FG_BLACK=$BLACK
+	FG_BLUE=$BLUE
+	FG_GREEN=$GREEN
+	FG_CYAN=$CYAN
+	FG_RED=$RED
+	FG_PURPLE=$PURPLE
+	FG_BROWN=$BROWN
+	FG_LIGHT_GRAY=$LIGHT_GRAY
+	FG_DARK_GRAY=$DARK_GRAY
+	FG_LIGHT_BLUE=$LIGHT_BLUE
+	FG_LIGHT_GREEN=$LIGHT_GREEN
+	FG_LIGHT_CYAN=$LIGHT_CYAN
+	FG_LIGHT_RED=$LIGHT_RED
+	FG_LIGHT_PURPLE=$LIGHT_PURPLE
+	FG_YELLOW=$YELLOW
+	FG_WHITE=$WHITE
+
+	BG_BLACK='\e[40m'
+	BG_RED='\e[41m'
+	BG_GREEN='\e[42m'
+	BG_BROWN='\e[43m'
+	BG_BLUE='\e[44m'
+	BG_PURPLE='\e[45m'
+	BG_CYAN='\e[46m'
+	BG_LIGHT_GRAY='\e[47m'
 
 # Style:
-FONT_BOLD='\033[1m'
-FONT_UNDERLINE='\033[4m'
+	FONT_NONE='\e[0m'
+	FONT_BOLD='\e[1m'
+	FONT_NORMAL='\e[2m'
 
-# Other:
-FONT_NONE='\033[00m'
+	FONT_UNDERLINE='\e[4m'
+	FONT_BLINK='\e[5m'
+	FONT_INVERT='\e[7m'
+
+fi
