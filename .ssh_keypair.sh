f9f790c079697b12f55d2f6b25892147705d2bc8
Michael Ambrus <michael.ambrus@gmail.com>
2017-03-04 16:14:56 +0100

    Code clean-up/beautification
    In preparation for RDA/DSA key option deployment (-r.-d)
    
    Change-Id: I2ea3e7f808d49d01ae117ad0607380efb529473b

--- .ssh_keypair.sh
+++ .ssh_keypair.sh
@@ -17,18 +17,22 @@ function create_srvscript() {
 	echo "TS=$2"											>>"$1"
 	echo -n 'echo "Auto-generated script '					>>"$1"
 	echo -n "$TS"											>>"$1"
-	echo 'Please remove if not in use"'						>>"$1"
+	echo '. Please remove if not in use"'					>>"$1"
 	echo "set -u"											>>"$1"
 	echo 'echo; echo'										>>"$1"
 	echo "if [ ! -d ~/.ssh ]; then"							>>"$1"
-	echo '	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
+	echo '	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~'\
+				 '~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
+
 	echo '	echo "Remote side [~/.ssh] directory missing."'	>>"$1"
 	echo '	echo "Initializing..."'							>>"$1"
-	echo '	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
+	echo '	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~'\
+				 '~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
 	echo "	ssh-keygen -t dsa"								>>"$1"
 	echo 'echo; echo'										>>"$1"
 	echo "fi;"												>>"$1"
-	echo 'echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
+	echo 'echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~'\
+				'~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
 	echo 'echo Adding transferred key id_dsa_${TS}.pub to authorized_keys2'	>>"$1"
 	echo "cd ~/.ssh"										>>"$1"
 	echo 'cat "/tmp/id_dsa_${TS}.pub" >> authorized_keys2'	>>"$1"
@@ -37,7 +41,8 @@ function create_srvscript() {
 	echo 'echo Removing temporary transferred key "/tmp/id_dsa_${TS}.pub"'	>>"$1"
 	echo 'rm "/tmp/id_dsa_${TS}.pub"'										>>"$1"
 	echo 'echo Server is removing script "/tmp/ssh_server_$TS.sh"'		>>"$1"
-	echo 'echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
+	echo 'echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~'\
+				'~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"'	>>"$1"
 	echo 'rm "/tmp/ssh_server_$TS.sh"'						>>"$1"
 	echo 'cd $OLDPWD'										>>"$1"
 	chmod 0777 "$1"
@@ -53,25 +58,31 @@ function local_key_copy() {
 
 	echo; echo
 	if [ ! -d ~/.ssh ]; then
-		echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+		echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+				"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 		echo "[~/.ssh] directory missing. Initializing..."
-		echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+		echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+				"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 		ssh-keygen -t dsa
 		echo; echo
 	fi;
 	if [ ! -f ~/.ssh/id_dsa.pub ]; then
-		echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+		echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+				"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 		echo "Public keyfile [~/.ssh/id_dsa.pub] missing. Creating one..."
-		echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+		echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+				"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 		ssh-keygen -t dsa -f ~/.ssh/id_dsa
 		echo; echo
 	fi;
 
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo "Transfering your public key to the server using the following command:"
 	echo "scp -P${PORT} ~/.ssh/id_dsa.pub ${USER}@${REMOTE}:/tmp/id_dsa_${TS}.pub"
 	echo "(You will need to enter password)"
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo; echo
 	scp -P${PORT} ~/.ssh/id_dsa.pub ${USER}@${REMOTE}:/tmp/id_dsa_${TS}.pub
 	echo; echo
@@ -113,30 +124,36 @@ function ssh_keypair() {
 	create_srvscript "/tmp/local_${SRV_SCRIPT}" "$TS"
 	local_key_copy "$REMOTE_USER" "$REMOTE_SERVER" "$REMOTE_PORT" "$TS"
 
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo "Transferring server-side script using the following command:"
-	echo -n -P"${REMOTE_PORT}" "scp \"/tmp/local_${SRV_SCRIPT}"
-	echo "${REMOTE_USER}@${REMOTE_SERVER}:/tmp/${SRV_SCRIPT}\""
+	echo	"scp \"/tmp/local_${SRV_SCRIPT}"\
+			"${REMOTE_USER}@${REMOTE_SERVER}:/tmp/${SRV_SCRIPT}\""
 	echo "(You will need to enter password)"
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo; echo
 	scp -P"${REMOTE_PORT}" "/tmp/local_${SRV_SCRIPT}" \
 		"${REMOTE_USER}@${REMOTE_SERVER}:/tmp/${SRV_SCRIPT}"
 	echo; echo
 
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo "Running server-side script using the following command:"
 	echo "ssh -p${REMOTE_PORT} \"${REMOTE_USER}@${REMOTE_SERVER}\" \"/tmp/${SRV_SCRIPT}\""
 	echo "(You will need to enter password)"
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	ssh -p${REMOTE_PORT} "${REMOTE_USER}@${REMOTE_SERVER}" "/tmp/${SRV_SCRIPT}"
 	echo; echo
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	echo "Key-pair binding with ${REMOTE_USER}@${REMOTE_SERVER} is done"
 	echo "You should now be able to log in without a password"
 	echo "If not, consider the workaround at this link:"
 	echo " https://bugs.launchpad.net/ubuntu/+source/openssh/+bug/201786"
-	echo "~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
+	echo 	"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"\
+			"~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~ ~-~"
 	sleep 2
 	#Done with it, remove local copy
 	echo "[$HOSTNAME] is removing script /tmp/local_${SRV_SCRIPT}"
